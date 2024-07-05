e72559b347383c2d2ae76fabbc63d71d48d32cea
kvnp89 <kvnp89@gmail.com>
2024-07-05 15:56:37 +0000

    sample data upload

--- codes/hello.py
+++ codes/hello.py
@@ -1,4 +1,6 @@
 import pandas as pd
 print("Hello CodeSpaces")
-df = pd.read_csv("D:\spark\pyspark_learning\data\Fire_Department_Calls_for_Service_20240103.csv")
-df
+import os
+print(os.getcwd())
+df = pd.read_csv("/workspaces/python_basics/data/sample.csv")
+df.head()
